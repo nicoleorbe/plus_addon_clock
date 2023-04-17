9a74d3562f25b77418ac5657de17cee68c04972c
Nicole Orbe <nicoleorbe@outlook.com>
2023-04-17 06:33:55 -0500

    added initial section for Country selection

---
+++ source/app.js
@@ -0,0 +1,31 @@
+// function showDate() {
+//   let date = document.querySelector("#date");
+//   date.innerHTML = new Date();
+// }
+
+// setTimeout(showDate, 5000);
+
+// function showMilliseconds() {
+//   let display = document.querySelector("#milliseconds");
+//   let date = new Date();
+//   let milliseconds = date.getMilliseconds();
+//   display.innerHTML = milliseconds;
+//   console.log(milliseconds);
+// }
+
+// showMilliseconds();
+// setInterval(showMilliseconds, 1);
+
+// function showSelectedColor(event) {
+//   if (event.target.value === "blue") {
+//     alert("💙");
+//   } else if (event.target.value === "red") {
+//     alert("❤️");
+//   } else if (event.target.value === "yellow") {
+//     alert("💛");
+//   }
+// }
+
+// let colorSelect = document.querySelector("#color-select");
+
+// colorSelect.addEventListener("change", showSelectedColor);
